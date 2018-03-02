5048e6d6a34bb7b7c7ae5732c5633e77327f0d9a
William-Kaiser <712677107@qq.com>
2018-03-02 10:19:38 +0800

    commit

--- src/main/java/com/lk1314/controller/HomeController.java
+++ src/main/java/com/lk1314/controller/HomeController.java
@@ -1,21 +1,14 @@
 package com.lk1314.controller;
 
-import com.lk1314.core.BaseController;
 import com.lk1314.entity.User;
-import com.lk1314.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 /**
  * @author liankai
@@ -23,6 +16,11 @@ import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping(value="/index",method=RequestMethod.GET)
+    public String index(){
+        return "/index";
+    }
 
     @RequestMapping(value="/login",method=RequestMethod.GET)
     public String loginForm(Model model){
